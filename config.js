50ae1095dcd2743a526a6e9eb73c44c40c369c85
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-10-04 16:44:59 +0200

    Add server.js and package

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '441908078718-q83ki3ld58briq7voclfu1ivp76pncud.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'e9UJD46e9CigvxsfHJcQ_5fJ',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
