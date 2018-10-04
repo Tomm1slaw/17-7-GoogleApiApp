ed089738b5458a46555c5d64a414449fa6dcd135
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-10-04 16:28:35 +0200

    Init project

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use('/store', function(req, res, next){
+    console.log('Hej, jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.get('/auth/google', function(req, res){
+	res.send('You are logged:)')
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
